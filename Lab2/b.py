7a7c28e7faf73f39748359b345295547164922d7
Manapaly <98648181+Manapaly@users.noreply.github.com>
2022-02-07 19:43:45 +0600

    Delete Lab2 directory

--- Lab2/b.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-a = list(map(int, input().split()))
-b=[]
-for i in range(len(a)):
-    for j in range(i+1,len(a)):
-        b.append(a[i]*a[j])
-
-print(max(b))
--- Lab2/zhai.py
+++
@@ -1,3 +0,0 @@
-n = list(map(str,input().split()))
-for i in sorted(n):
-    print(i)
