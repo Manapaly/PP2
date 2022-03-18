e6c769237e386b3a81a8777d7e18f227f37623ef
ERNAT <98648181+Manapaly@users.noreply.github.com>
2022-03-18 21:37:08 +0600

    Delete 4.py

--- Lab6/Builtin/4.py
+++
@@ -1,8 +0,0 @@
-#4 Python program that invoke square root function after specific milliseconds.
-
-from time import sleep
-from math import sqrt
-a = int(input())
-b = int(input())
-sleep(b/1000)
-print(f'Square root of {a} after {b} miliseconds is {sqrt(a)}')
